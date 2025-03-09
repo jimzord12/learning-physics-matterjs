b4ac294dbfae7186acb7e154bd6529d06356e87d
Dimitrios Stamatakis <45168324+jimzord12@users.noreply.github.com>
2025-03-09 17:27:08 +0200

    Update sketch-lesson-04.js

--- src/sketches/level-04/sketch-lesson-04.js
+++ src/sketches/level-04/sketch-lesson-04.js
@@ -8,7 +8,7 @@
   let bridge;
   let handPose;
   let video;
-  let hand = { index: null, pinky: null };
+  let hand = { index_1: null, index_2: null };
 
   const CANVAS_W = 800;
   const CANVAS_H = 640;
@@ -27,7 +27,7 @@
   preload = function () {
     // Load the handPose model
     handPose = ml5.handPose({
-      maxHands: 1,
+      maxHands: 2,
       flipped: true,
     });
     console.log('ml5 handPose: ', handPose);
@@ -43,11 +43,16 @@
     video.hide();
     // start detecting hands from the webcam video
     handPose.detectStart(video, (data) => {
-      if (data && data[0] && data[0]['index_finger_tip']) {
-        const index = data[0]['index_finger_tip'];
-        const pinky = data[0]['pinky_finger_tip'];
-        // console.log(data[0]['index_finger_tip']);
-        hand = { index, pinky };
+      if (
+        data &&
+        data[0] &&
+        data[1] &&
+        data[0]['index_finger_tip'] &&
+        data[1]['index_finger_tip']
+      ) {
+        const index_1 = data[0]['index_finger_tip'];
+        const index_2 = data[1]['index_finger_tip'];
+        hand = { index_1, index_2 };
       }
     });
 
@@ -59,12 +64,12 @@
     image(video, 0, 0, width, height);
     Engine.update(engine);
 
-    const indexFinger = hand.index;
-    const pinkyFinger = hand.pinky;
-    if (!(indexFinger && pinkyFinger)) return;
+    const indexFinger1 = hand.index_1;
+    const indexFinger2 = hand.index_2;
+    if (!(indexFinger1 && indexFinger2)) return;
 
-    bridge.changeFirstBallPosition(indexFinger.x, indexFinger.y);
-    // bridge.changeLastBallPosition(pinkyFinger.x, pinkyFinger.y);
+    bridge.changeFirstBallPosition(indexFinger1.x, indexFinger1.y);
+    bridge.changeLastBallPosition(indexFinger2.x, indexFinger2.y);
 
     bridge.show();
   };
